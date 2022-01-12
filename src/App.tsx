986ad2d1819502a694c90c6023de864d1df20a32
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-01-12 14:27:49 +0100

    app rendering button using bootsrap grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import IpButton from "./components/IpButton";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const App = () => {
-  return <div className="App">Hello World!</div>;
+  return (
+    <Container className="mt-5" fluid>
+      <Row style={{ justifyContent: "center" }}>
+        <Col className="text-center" xs={2}>
+          <IpButton />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default App;
